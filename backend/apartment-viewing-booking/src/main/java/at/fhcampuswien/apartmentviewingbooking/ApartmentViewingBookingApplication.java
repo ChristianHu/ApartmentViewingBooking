ebdda3300654671795318991b01c4fd035e1b39d
Mohammad Alsalkini <eng.salkini@hotmail.com>
2022-11-05 14:49:43 +0100

    initial backend commit

---
+++ backend/apartment-viewing-booking/src/main/java/at/fhcampuswien/apartmentviewingbooking/ApartmentViewingBookingApplication.java
@@ -0,0 +1,13 @@
+package at.fhcampuswien.apartmentviewingbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentViewingBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmentViewingBookingApplication.class, args);
+	}
+
+}
---
+++ backend/apartment-viewing-booking/src/test/java/at/fhcampuswien/apartmentviewingbooking/ApartmentViewingBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhcampuswien.apartmentviewingbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentViewingBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
