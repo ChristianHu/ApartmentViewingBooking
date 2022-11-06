0edcf2767c7506f03478dff6177484813ca28dff
Alireza Esbati <43641705+Alirez14@users.noreply.github.com>
2022-11-06 18:05:04 +0100

    apartment  card

---
+++ frontend/components/com-apartment-list-card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ComApartmentListCard = ({ PropertyName, PropertyPrice }) => {
+	return (
+		<div className="card">
+			<div>{PropertyName}</div>
+			<div>{PropertyPrice}</div>
+		</div>
+	)
+}
+
+export default ComApartmentListCard
