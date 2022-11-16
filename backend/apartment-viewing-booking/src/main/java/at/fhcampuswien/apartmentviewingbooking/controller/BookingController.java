1aea43ce74492e920f7c9efa79e4160f141fe3f9
LAPTOP-JAEFD5TU\lucas <lucaswizard98@gmail.com>
2022-11-16 18:51:06 +0100

    + definet Rest Methods in booking Service

--- backend/apartment-viewing-booking/src/main/java/at/fhcampuswien/apartmentviewingbooking/controller/BookingController.java
+++ backend/apartment-viewing-booking/src/main/java/at/fhcampuswien/apartmentviewingbooking/controller/BookingController.java
@@ -1,4 +1,36 @@
 package at.fhcampuswien.apartmentviewingbooking.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/apartment/booking")
 public class BookingController {
+
+    @PostMapping("/user/{userId}/flat/{flatId}")
+    public ResponseEntity<?> bookFlat(@PathVariable long userId, @PathVariable long flatId) {
+        //TODO
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping("/user/{userID}")
+    public ResponseEntity<?> getBookingByUser(@PathVariable long userId) {
+        //TODO
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping("/flat/{flatID}")
+    public ResponseEntity<?> getBookingByFlat(@PathVariable long flatID) {
+        //TODO
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @DeleteMapping("/user/{userId}/flat/{flatId}")
+    public ResponseEntity<?> deleteBooking(@PathVariable long userId, @PathVariable long flatId) {
+        //TODO
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+
 }
