67ed2f7f3705df9114a3daa4167be8cba46c93f7
Alireza Esbati <43641705+Alirez14@users.noreply.github.com>
2022-11-17 21:00:05 +0100

    fixed the login form with react hook form

--- frontend/components/com-login-form.js
+++ frontend/components/com-login-form.js
@@ -1,3 +1,4 @@
+import { ErrorMessage } from "@hookform/error-message";
 import React from "react";
 import { useForm } from "react-hook-form";
 
@@ -7,7 +8,9 @@ const ComLoginForm = () => {
 		handleSubmit,
 		formState: { errors },
 	} = useForm();
-	const onSubmit = (data) => console.log(data);
+	const onSubmit = (data) => {
+		console.log(data);
+	};
 	console.log(errors);
 
 	return (
@@ -15,12 +18,43 @@ const ComLoginForm = () => {
 			<form onSubmit={handleSubmit(onSubmit)}>
 				<div className="flex flex-col pt-[18px]">
 					<label className="pb-[4px]">Email</label>
-					<input type="email" placeholder="email" {...register("email", { required: true })} />
-					{/* <div className="h-[20px] m-auto text-red-500">{renderErrorMessage("email")}</div> */}
+					<input
+						className={
+							errors.email
+								? "h-[40px] rounded-[8px] bg-[#F2F2F2] p-[12px] border-[2px] border-red-500"
+								: "h-[40px] rounded-[8px] bg-[#F2F2F2] p-[12px]"
+						}
+						type="text"
+						{...register("email", {
+							required: "is required",
+							pattern: {
+								value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+								message: "invalid email address",
+							},
+						})}
+					/>
+					<ErrorMessage
+						errors={errors}
+						name="email"
+						render={({ message }) => <small className="text-red-500">{message}</small>}
+					/>
 				</div>
 				<div className="flex flex-col pt-[18px]">
-					<label className="pb-[4px]">Email</label>
-					<input type="undefined" placeholder="password" {...register} />
+					<label className="pb-[4px]">Password</label>
+					<input
+						className={
+							errors.password
+								? "h-[40px] rounded-[8px] bg-[#F2F2F2] p-[12px] border-[2px] border-red-500"
+								: "h-[40px] rounded-[8px] bg-[#F2F2F2] p-[12px]"
+						}
+						type="password"
+						{...register("password", { required: "is required" })}
+					/>
+					<ErrorMessage
+						errors={errors}
+						name="password"
+						render={({ message }) => <small className="text-red-500">{message}</small>}
+					/>
 				</div>
 				<div className="flex flex-col pt-[18px]">
 					<button className="py-[18px] underline">Forgot Password?</button>
