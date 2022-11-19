29c0c2c6e75d59757d14fbef6d0dd57fc3996057
Christian Hüttler <christian.hu6@gmail.com>
2022-11-19 20:22:38 +0100

    Added login.js

---
+++ frontend/pages/login.js
@@ -0,0 +1,23 @@
+import {useState} from 'react';
+import ComLoginForm from "../components/com-login-form";
+
+export default function Login() {
+	const [showLogin, setShowLogin] = useState(true)
+	const handleLogin = () => {
+		setShowLogin(true)
+	}
+	const handleRegister = () => {
+		setShowLogin(false)
+	}
+	return (
+		<div>
+			<div>
+				<button onClick={handleLogin}>Login</button>
+				<button onClick={handleRegister}>Register</button>
+			</div>
+			<div>
+				{showLogin ? <ComLoginForm/> : <p>Register</p>}
+			</div>
+		</div>
+	);
+}
