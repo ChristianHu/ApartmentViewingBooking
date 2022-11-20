b25eadaf588516eb9d296a960cd2de2fc801c77a
LAPTOP-JAEFD5TU\lucas <lucaswizard98@gmail.com>
2022-11-20 17:43:09 +0100

    + added FlatService: get flat by id function

---
+++ backend/apartment-viewing-booking/src/main/java/at/fhcampuswien/apartmentviewingbooking/service/flatService/FlatService.java
@@ -0,0 +1,23 @@
+package at.fhcampuswien.apartmentviewingbooking.service.flatService;
+
+import at.fhcampuswien.apartmentviewingbooking.model.flat.Flat;
+import at.fhcampuswien.apartmentviewingbooking.repository.FlatRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class FlatService {
+
+    private FlatRepository flatRepository;
+
+    @Autowired
+    public FlatService(FlatRepository flatRepository){
+        this.flatRepository=flatRepository;
+    }
+
+    public Optional<Flat> getFlatByID(long flatId){
+       return flatRepository.findById(flatId);
+    }
+}
