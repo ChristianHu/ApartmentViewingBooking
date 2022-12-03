714ca664bc22c2cd041ca5e1afc1fce2c3a58f02
Christian Hüttler <christian.hu6@gmail.com>
2022-12-03 15:11:30 +0100

    Enhanced the ui of com-apartment-card.js to show which part of the card is clickable

--- frontend/components/com-apartment-card.js
+++ frontend/components/com-apartment-card.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React from "react";
 import ComApartmentFacts from "./com-apartment-facts";
 import ComImageSlider from "./com-image-slider";
+import Image from "next/image";
 
 const ComApartmentCard = ({
 	propertyId,
@@ -15,15 +16,24 @@ const ComApartmentCard = ({
 		<div className="apartment-card flex flex-col md:flex-row rounded-[16px] bg-[#F2F2F2] w-4/5 my-6">
 			<ComImageSlider className="m-auto h-1/2 md:w-1/2" images={propertyImages} />
 			<div className="h-1/2 md:w-1/2">
-				<Link href={"/apartment/" + propertyId}>
-					<ComApartmentFacts
-						className="card-text-section p-[20px] h-full"
-						propertyAddress={propertyAddress}
-						propertyRoomNumber={propertyRoomNumber}
-						propertySize={propertySize}
-						propertyPrice={propertyPrice}
-					></ComApartmentFacts>
-				</Link>
+				<div className="m-[20px]">
+					<Link href={"/apartment/" + propertyId}>
+						<ul className="flex flex-row bg-[#D7D7D7] rounded-[8px] ">
+							<li>
+								<ComApartmentFacts
+									className="card-text-section p-[20px] h-full"
+									propertyAddress={propertyAddress}
+									propertyRoomNumber={propertyRoomNumber}
+									propertySize={propertySize}
+									propertyPrice={propertyPrice}
+								/>
+							</li>
+							<li>
+								<Image width="12" height="24" src="/icon_arrow_right.svg" className="object-contain h-[24px] w-[12px] mt-[20px] mr-[10px]" alt="Arrow right image"/>
+							</li>
+						</ul>
+					</Link>
+				</div>
 			</div>
 		</div>
 	);
