9c2a8c2ce86e9510068a18017c69947654f7da38
Christian Hüttler <christian.hu6@gmail.com>
2022-12-03 18:39:39 +0100

    Added navbar to profile.js

--- frontend/pages/profile.js
+++ frontend/pages/profile.js
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import {mockApartments} from "../mocks/mock-apartments";
 import ComBookingCard from "../components/com-booking-card";
 import ComPersonalDataForm from "../components/com-personaldata-form";
+import LayGeneral from "../layouts/lay-general";
 
 export default function Profile() {
 	const [showMyBookings, setShowMyBookings] = useState(true)
@@ -13,17 +14,19 @@ export default function Profile() {
 	}
 	return (
 		<div>
-			<div>
-				<button onClick={handleMyBookings}>My Bookings</button>
-				<button onClick={handlePersonalData}>Personal Data</button>
-			</div>
-			<div>
-				{showMyBookings ? <div>
-					{mockApartments.map((apartment, index) => (
-						<ComBookingCard key={index} {...apartment} />
-					))}
-				</div> : <ComPersonalDataForm/>}
-			</div>
+			<LayGeneral>
+				<div>
+					<button onClick={handleMyBookings}>My Bookings</button>
+					<button onClick={handlePersonalData}>Personal Data</button>
+				</div>
+				<div>
+					{showMyBookings ? <div>
+						{mockApartments.map((apartment, index) => (
+							<ComBookingCard key={index} {...apartment} />
+						))}
+					</div> : <ComPersonalDataForm/>}
+				</div>
+			</LayGeneral>
 		</div>
 	);
 }
