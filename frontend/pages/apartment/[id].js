1a325d38fefbfd6d32d19fa45a4266f352a1e9f4
Christian Hüttler <christian.hu6@gmail.com>
2022-12-03 15:09:36 +0100

    Added star rating to apartment details page

--- frontend/pages/apartment/[id].js
+++ frontend/pages/apartment/[id].js
@@ -1,8 +1,26 @@
 import ComApartmentFacts from "../../components/com-apartment-facts";
 import ComImageSlider from "../../components/com-image-slider";
 import LayGeneral from "../../layouts/lay-general";
+import {useState} from "react";
 
 export default function Details({ propertyId }) {
+	const [rating, setRating] = useState(0)
+	const handleRating = (number) => {
+		setRating(number)
+	}
+	
+	const renderRatingStars = () => {
+		let result = []
+		for (let i = 0; i < 5; i++) {
+			if (rating === i) {
+				result.push(<input key={i} type="radio" name="rating-1" className="mask mask-star" checked />)
+			} else {
+				result.push(<input key={i} type="radio" name="rating-1" className="mask mask-star" onClick={() => handleRating(i)}/>)
+			}
+		}
+		return result
+	}
+	
 	const handleBook = () => {
 		console.log("TODO: show booking page");
 	};
@@ -21,11 +39,11 @@ export default function Details({ propertyId }) {
 							]}
 						/>
 					</div>
-					<button className="btn px-[70px]" onClick={handleBook}>
+					<button className="flex flex-row m-auto btn px-[70px]" onClick={handleBook}>
 						Book
 					</button>
 					<ComApartmentFacts
-						className=""
+						className="my-[18px]"
 						propertyAddress={propertyId}
 						propertyRoomNumber={propertyId}
 						propertySize={"Test"}
@@ -38,6 +56,16 @@ export default function Details({ propertyId }) {
 							labore et dolore magna aliqua.
 						</p>
 					</div>
+					<div className="my-[18px]">PLACEHOLDER for map</div>
+					<ul className="flex flex-row my-[18px]">
+						<li>
+							<h3 className="mr-[15px]">Rate this apartment: </h3>
+						</li>
+						<li>
+							<div className="rating"> { renderRatingStars() }</div>
+						</li>
+					</ul>
+					<div className="my-[18px]">PLACEHOLDER for comment section</div>
 				</div>
 			</LayGeneral>
 		</div>
