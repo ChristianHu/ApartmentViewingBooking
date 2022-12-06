7f8927a4fa2dd723551d4232e69556a0ac867ae0
Christian Hüttler <christian.hu6@gmail.com>
2022-12-06 20:41:30 +0100

    Added 'username' to login/register form

--- frontend/components/com-login-form.js
+++ frontend/components/com-login-form.js
@@ -16,25 +16,25 @@ const ComLoginForm = () => {
 		<div className="m-auto max-w-[400px] px-[20px] my-[20px]">
 			<form onSubmit={handleSubmit(onSubmit)}>
 				<div className="flex flex-col pt-[18px]">
-					<label className="pb-[4px]">Email</label>
+					<label className="pb-[4px]">Username</label>
 					<input
 						className={
-							errors.email
+							errors.username
 								? "h-[40px] rounded-[8px] bg-[#F2F2F2] p-[12px] border-[2px] border-red-500"
 								: "h-[40px] rounded-[8px] bg-[#F2F2F2] p-[12px]"
 						}
 						type="text"
-						{...register("email", {
+						{...register("username", {
 							required: "is required",
 							pattern: {
-								value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-								message: "Invalid email address",
+								value: /^[^0-9]{3,}$/i,
+								message: "Invalid name",
 							},
 						})}
 					/>
 					<ErrorMessage
 						errors={errors}
-						name="email"
+						name="username"
 						render={({ message }) => <small className="text-red-500">{message}</small>}
 					/>
 				</div>
@@ -57,7 +57,6 @@ const ComLoginForm = () => {
 				</div>
 				<div className="flex flex-col pt-[18px]">
 					<button className="py-[18px] underline">Forgot Password?</button>
-
 					<input className="btn" type="submit" />
 				</div>
 			</form>
