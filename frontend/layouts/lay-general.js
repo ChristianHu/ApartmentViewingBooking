6edea307de0445184034eae6a3956151a2ccbc05
Alireza Esbati <43641705+Alirez14@users.noreply.github.com>
2022-12-08 22:40:07 +0100

    fix footer

--- frontend/layouts/lay-general.js
+++ frontend/layouts/lay-general.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import ComFooter from '../components/com-footer'
-import ComNav from '../components/com-nav'
+import React from "react";
+import ComFooter from "../components/com-footer";
+import ComNav from "../components/com-nav";
 
 const LayGeneral = (props) => {
 	return (
 		<>
 			<ComNav></ComNav>
-			{props.children}
+			<div className="min-h-screen">{props.children}</div>
 			<ComFooter></ComFooter>
 		</>
-	)
-}
+	);
+};
 
-export default LayGeneral
+export default LayGeneral;
